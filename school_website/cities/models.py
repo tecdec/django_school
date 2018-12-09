719a13d3905918960337b30e5dd59ae5d65989aa
tecdec <declan_lambe@hotmail.com>
2018-12-09 02:08:28 +0000

    solved issue with Question has no _state" "

--- school_website/cities/models.py
+++ school_website/cities/models.py
@@ -19,29 +19,32 @@ class CategoryManager(models.Manager):
 
 
 class Category(models.Model):
-    category = models.CharField(max_length=50, blank=True,
+    category = models.CharField(max_length=50, blank=False,
                                 unique=True, null=True)
     # create categories with :category = Category.objects.create_category("cities")
+    # objects = models.Manager()
     objects = CategoryManager()
 
     def __str__(self):
         return self.category
 
-class CitiesOfTheWorld(models.Model):
-    city=models.CharField(max_length=50, blank=False,
-                                unique=True, null=False)
-    country=models.CharField(max_length=50, blank=False,
-                                unique=True, null=False)
-    continent=models.CharField(max_length=50, blank=False,
-                                unique=True, null=False)
 
-    def __init__(self,city,country,continent):
-        self.city=city
-        self.country=country
-        self.continent=continent
+class CitiesOfTheWorld(models.Model):
+    city = models.CharField(max_length=50, blank=False,
+                            unique=True, null=False)
+    country = models.CharField(max_length=50, blank=False,
+                               unique=True, null=False)
+    continent = models.CharField(max_length=50, blank=False,
+                                 unique=True, null=False)
+
+    def __init__(self, city, country, continent):
+        self.city = city
+        self.country = country
+        self.continent = continent
 
     def __str__(self):
         return self.city
+
 
 class ExamManager(models.Manager):
     def create_exam(self, name, allowed_time_mins=10, category=None):
@@ -67,42 +70,43 @@ class Exam(models.Model):
     def __str__(self):
         return "--" + self.name + \
                " Allowed_time_mins:" + str(self.allowed_time_mins) + \
-               " Category: " + str(self.category) +"\n"
+               " Category: " + str(self.category) + "\n"
+
 
 class Answer(models.Model):
     answer = models.CharField(max_length=200)
-    #answer = models.TextField()
-    #question = models.ForeignKey(Question, blank=True, null=True, on_delete=models.CASCADE)
+    # answer = models.TextField()
+    # question = models.ForeignKey(Question, blank=True, null=True, on_delete=models.CASCADE)
 
     objects = models.Manager()
+
     def __str__(self):
         return self.answer
 
 
-
 class QuestionManager(models.Manager):
     def question_category(self, category):
-        return self.filter(title__icontains=category)
+        return self.filter(category__icontains=category)
 
-
+    def create_question(self, question_text, correct_answer,
+                        category):
+        question = self.create(question_text=question_text,
+                               correct_answer=correct_answer,
+                               category=category)
+        return question
 
 
 class Question(models.Model):
-    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, blank=False, null=True, on_delete=models.CASCADE)
+
     exams = models.ManyToManyField(Exam)
     question_text = models.CharField(max_length=200)
     correct_answer = models.CharField(max_length=50)
     answer_given = models.CharField(max_length=50, blank=True, null=True)
     created_date = models.DateTimeField(timezone.now(), blank=True, null=True)
-    answers = models.ForeignKey(Answer, on_delete=models.CASCADE,  blank=True, null=True)
-    objects = models.Manager()
-    category_objects = QuestionManager()
+    answers = models.ForeignKey(Answer, on_delete=models.CASCADE, blank=True, null=True)
+    objects = QuestionManager()
 
-    def __init__(self,question_text, correct_answer,
-                      category):
-        self.question_text = question_text
-        self.correct_answer = correct_answer
-        self.category = category
 
     def __str__(self):
         return self.question_text
@@ -110,8 +114,8 @@ class Question(models.Model):
     def is_correct(self, correct_answer, answer_given):
         return correct_answer == answer_given
 
-    def submit_answer(self,answer_given):
-        self.answer_given=answer_given
+    def submit_answer(self, answer_given):
+        self.answer_given = answer_given
         self.save()
 
 
@@ -122,17 +126,17 @@ class QuestionGenerator(models.Model):
              question_subject = "Ireland"?
 
     '''
-    def __init__(self,category, question_predicate,question_subject_list,question_choices_list, num_questions_to_select=10,
+
+    def __init__(self, category, question_predicate, question_subject_list, question_choices_list,
+                 num_questions_to_select=10,
                  num_choices_to_select=4):
         question_subject_list_randomized = random.sample(question_subject_list, num_questions_to_select)
         first_random_ = question_subject_list_randomized[0]
         for question_subject in question_subject_list_randomized:
             question_text = question_predicate + question_subject + "?"
             correct_answer = "dublin"
-            Question(question_text,correct_answer,category)
-
-
+            Question(question_text, correct_answer, category)
 
 
 class Choices(models.Model):
-    question = models.ForeignKey(Question,on_delete=models.CASCADE,  blank=True, null=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, blank=True, null=True)
